952a394712c53b82621195cdc1c0095148427de3
Venkat Vinakollu <venkat.vinakollu@ggktech.local>
2016-02-24 17:57:28 +0530

    New

--- chef-repo/cookbooks/windows_webserver/recipes/default.rb
+++ chef-repo/cookbooks/windows_webserver/recipes/default.rb
@@ -23,3 +23,55 @@ dsc_script 'Web-Asp-Net45' do
   }
   EOH
 end
+
+dsc_script 'Download-Artifacts' do
+  code <<-EOH
+  Script InstallAntivirus{
+    GetScript={ $true }
+    SetScript={
+      $source = "https://s3-us-west-1.amazonaws.com/ashishbucket/MvcApplication2.zip"
+      $destination = "C:\\MvcApplication2.zip"
+      Invoke-WebRequest $source -OutFile $destination
+    }
+    TestScript={
+      $false
+        }
+  }
+  EOH
+end
+
+dsc_script 'Download-Artifacts' do
+  code <<-EOH
+  Script Download-Artifacts{
+    GetScript={ $true }
+    SetScript={
+      $source = "https://s3-us-west-1.amazonaws.com/ashishbucket/MvcApplication2.zip"
+      $destination = "C:\\MvcApplication2.zip"
+      Invoke-WebRequest $source -OutFile $destination
+    }
+    TestScript={
+      $false
+        }
+  }
+  EOH
+end
+
+dsc_script 'Delete-Current-Website' do
+  code <<-EOH
+  File Website-Del {
+    DestinationPath = "C:\\inetpub\\wwwroot\\*"
+    Ensure = "Absent"
+    Recurse = $true
+  }
+  EOH
+end
+
+dsc_script 'Extract-Artifacts' do
+  code <<-EOH
+  Archive Artifact {
+      Ensure = "Present"
+      Path = "C:\\MvcApplication2.zip"
+      Destination = "C:\\inetpub\\wwwroot\\"
+  }
+  EOH
+end
